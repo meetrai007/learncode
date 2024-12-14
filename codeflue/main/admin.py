877c8d0466dad17a1c874168672f7f238d13e633
meetrai007 <meetrai101@gmail.com>
2024-12-14 13:56:06 +0530

    display fields in order in admin pannel

--- codeflue/main/admin.py
+++ codeflue/main/admin.py
@@ -8,16 +8,19 @@ admin.site.register(Language)
 class TopicAdmin(admin.ModelAdmin):  # Use PascalCase for class names
     model = Topic
     list_display = ('language', 'title', 'order')  # Specify fields to display
-
+    ordering = ('language', 'order')  # Sort first by language, then by order
+    
 # Custom admin class for SubTopic
 class SubTopicAdmin(admin.ModelAdmin):  # Use PascalCase for class names
     model = SubTopic
     list_display = ('topic', 'title', 'order')  # Specify fields to display
+    
 
 # Custom admin class for Content
 class ContentAdmin(admin.ModelAdmin):  # Use PascalCase for class names
     model = Content
-    list_display = ('topic', 'subtopic', 'title')  # Specify fields to display
+    list_display = ('language','topic', 'subtopic', 'title')  # Specify fields to display
+    ordering = ('language','topic', 'subtopic', 'title')
 
 # Register models with their respective admin classes
 admin.site.register(Topic, TopicAdmin)
